eb99b8743556759f5769398cd26130aea01feb9c
Liam Hanrahan <outercloudstudio@gmail.com>
2026-07-02 01:25:20 -0400

    improve alu ready semantics

--- src/main/scala/RISCV/processing_elements/Alu.scala
+++ src/main/scala/RISCV/processing_elements/Alu.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 import _root_.circt.stage.ChiselStage
 
-class ALU(val width: Int = 32) extends Module {
+class Alu(val width: Int = 32) extends Module {
     val io = IO(new Bundle {
         val instruction = Input(new InstructionBundle())
         val valid = Input(Bool())
@@ -12,18 +12,18 @@ class ALU(val width: Int = 32) extends Module {
         val out = Output(new InstructionBundle())
         val out_valid = Output(Bool())
 
-        val consume = Input(Bool())
         val ready = Output(Bool())
+        val next_ready = Input(Bool())
     })
 
-    val out = RegInit(new InstructionBundle())
+    val out = RegInit(0.U.asTypeOf(new InstructionBundle))
     io.out := out
     val out_valid = RegInit(false.B)
     io.out_valid := out_valid
 
-    io.ready := io.consume
+    io.ready := io.next_ready
 
-    when(io.consume) {
+    when(io.next_ready) {
         out := io.instruction
         out_valid := io.valid
 
@@ -32,7 +32,7 @@ class ALU(val width: Int = 32) extends Module {
                 switch(io.instruction.func3) {
                     // ADDI
                     is("b000".U) {
-                        out.rd_value := instruction.rs1_value + io.instruction.immediate
+                        out.rd_value := io.instruction.rs1_value + io.instruction.immediate
                     }
                 }
             }
