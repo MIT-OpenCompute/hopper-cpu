29847205ecc529d98ac8013d7e14eada66452800
Liam Hanrahan <outercloudstudio@gmail.com>
2026-07-04 15:17:56 -0400

    fix simultaneous mark in idq

--- src/main/scala/RISCV/core/InstructionDispatchQueue.scala
+++ src/main/scala/RISCV/core/InstructionDispatchQueue.scala
@@ -20,24 +20,26 @@ class InstructionDispatchQueue() extends Module {
 
         val ready = Output(Bool())
 
-        val broadcast_valid = Input(Bool())
-        val broadcast_register = Input(UInt(5.W))
-        val broadcast_value = Input(UInt(32.W))
+        val broadcast_free_valid = Input(Bool())
+        val broadcast_free_register = Input(UInt(5.W))
+        val broadcast_free_value = Input(UInt(32.W))
+
+        val broadcast_mark_valid = Output(Bool())
+        val broadcast_mark_register = Output(UInt(5.W))
     })
 
     val queue = RegInit(VecInit(Seq.fill(8)(0.U.asTypeOf(new QueueEntry))))
 
-    val last_entry = WireInit(0.U(3.W))
-
+    val last_free_entry = WireInit(0.U(3.W))
     for (n <- 0 to 7) {
         when(queue(n).valid) {
-            last_entry := n.U(3.W)
+            last_free_entry := n.U(3.W) + 1.U
         }
     }
+    val full = queue(7.U).valid
 
     val first_valid_entry = WireInit(0.U(3.W))
     val first_valid_entry_valid = WireInit(false.B)
-
     for (n <- 0 to 7) {
         when(queue(7.U - n.U).valid && queue(7.U - n.U).instruction.rs1_valid && queue(7.U - n.U).instruction.rs2_valid && io.alu_ready) {
             first_valid_entry := 7.U - n.U
@@ -45,15 +47,15 @@ class InstructionDispatchQueue() extends Module {
         }
     }
 
-    when(io.broadcast_valid) {
+    when(io.broadcast_free_valid) {
         for (n <- 0 to 7) {
-            when(queue(n.U).instruction.rs1 === io.broadcast_register) {
-                queue(n.U).instruction.rs1_value := io.broadcast_value
+            when(queue(n.U).instruction.rs1 === io.broadcast_free_register) {
+                queue(n.U).instruction.rs1_value := io.broadcast_free_value
                 queue(n.U).instruction.rs1_valid := true.B
             }
 
-            when(queue(n.U).instruction.rs2 === io.broadcast_register) {
-                queue(n.U).instruction.rs2_value := io.broadcast_value
+            when(queue(n.U).instruction.rs2 === io.broadcast_free_register) {
+                queue(n.U).instruction.rs2_value := io.broadcast_free_value
                 queue(n.U).instruction.rs2_valid := true.B
             }
         }
@@ -62,10 +64,18 @@ class InstructionDispatchQueue() extends Module {
     io.alu_out := queue(first_valid_entry).instruction
     io.alu_out_valid := first_valid_entry_valid
 
+    val broadcast_mark_valid = WireInit(false.B)
+    io.broadcast_mark_valid := broadcast_mark_valid
+    val broadcast_mark_register = WireInit(0.U)
+    io.broadcast_mark_register := broadcast_mark_register
+
     when(first_valid_entry_valid) {
         for (n <- 1 to 7) {
             when(n.U > first_valid_entry) {
                 queue((n - 1).U) := queue(n.U)
+
+                broadcast_mark_valid := true.B
+                broadcast_mark_register := queue(first_valid_entry).instruction.rd
 
                 when(queue(n.U).instruction.rs1 === queue(first_valid_entry).instruction.rd) {
                     queue((n - 1).U).instruction.rs1_valid := false.B
@@ -80,19 +90,47 @@ class InstructionDispatchQueue() extends Module {
         queue(7.U).valid := false.B
     }
 
-    io.ready := last_entry < 7.U
+    io.ready := !full
 
-    when(io.valid && last_entry < 7.U) {
+    when(io.valid && !full) {
         when(first_valid_entry_valid) {
-            queue(last_entry - 1.U).instruction := io.instruction
-            queue(last_entry - 1.U).valid := true.B
+            queue(last_free_entry - 1.U).instruction := io.instruction
+            queue(last_free_entry - 1.U).valid := true.B
+
+            when(broadcast_mark_valid && broadcast_mark_register === io.instruction.rs1) {
+                queue(last_free_entry - 1.U).instruction.rs1_valid := false.B
+            }
+
+            when(broadcast_mark_valid && broadcast_mark_register === io.instruction.rs2) {
+                queue(last_free_entry - 1.U).instruction.rs2_valid := false.B
+            }
         }.otherwise {
-            queue(last_entry).instruction := io.instruction
-            queue(last_entry).valid := true.B
+            queue(last_free_entry).instruction := io.instruction
+            queue(last_free_entry).valid := true.B
+
+            when(broadcast_mark_valid && broadcast_mark_register === io.instruction.rs1) {
+                queue(last_free_entry).instruction.rs1_valid := false.B
+            }
+
+            when(broadcast_mark_valid && broadcast_mark_register === io.instruction.rs2) {
+                queue(last_free_entry).instruction.rs2_valid := false.B
+            }
         }
     }
 
     // printf("\n\n")
+
+    // printf(
+    //   "First valid entry: %d First valid entry valid: %b Valid: %b\n",
+    //   first_valid_entry,
+    //   first_valid_entry_valid,
+    //   io.valid
+    // )
+
+    // printf(
+    //   "Last free entry: %d\n",
+    //   last_free_entry
+    // )
 
     // for (n <- 0 to 7) {
     //     printf(
--- src/test/scala/RISCV/InstructionDispatchQueueTest.scala
+++ src/test/scala/RISCV/InstructionDispatchQueueTest.scala
@@ -28,26 +28,89 @@ class InstructionDispatchQueueTest extends AnyFreeSpec with Matchers with Chisel
 
             dut.io.alu_ready.poke(true.B)
 
-            dut.io.broadcast_valid.poke(false.B)
-            dut.io.broadcast_register.poke(0.U)
-            dut.io.broadcast_value.poke(0.U)
+            dut.io.broadcast_free_valid.poke(false.B)
+            dut.io.broadcast_free_register.poke(0.U)
+            dut.io.broadcast_free_value.poke(0.U)
 
             dut.clock.step(1)
 
             dut.io.valid.poke(false.B)
 
-            dut.io.broadcast_valid.poke(true.B)
-            dut.io.broadcast_register.poke(1.U)
-            dut.io.broadcast_value.poke(2.U)
+            dut.io.broadcast_free_valid.poke(true.B)
+            dut.io.broadcast_free_register.poke(1.U)
+            dut.io.broadcast_free_value.poke(2.U)
 
             dut.clock.step(1)
 
-            dut.io.broadcast_valid.poke(false.B)
+            dut.io.broadcast_free_valid.poke(false.B)
 
             dut.io.alu_out_valid.expect(true.B)
             dut.io.alu_out.opcode.expect("b0010011".U)
 
+            dut.io.broadcast_mark_valid.expect(true.B)
+            dut.io.broadcast_mark_register.expect(3.U)
+
             dut.clock.step(1)
+
+            dut.io.alu_out_valid.expect(false.B)
+
+            dut.clock.step(1)
+        }
+    }
+
+    "IDQ test simultaneous mark" in {
+        simulate(new InstructionDispatchQueue()) { dut =>
+            dut.io.instruction.rs1.poke(1.U)
+            dut.io.instruction.rs1_value.poke(1.U)
+            dut.io.instruction.rs1_valid.poke(false.B)
+            dut.io.instruction.rs2.poke(0.U)
+            dut.io.instruction.rs2_value.poke(0.U)
+            dut.io.instruction.rs2_valid.poke(true.B)
+            dut.io.instruction.rd.poke(3.U)
+            dut.io.instruction.rd_value.poke(0.U)
+            dut.io.instruction.immediate.poke(3.U)
+            dut.io.instruction.opcode.poke("b0010011".U)
+            dut.io.instruction.func3.poke(0.U)
+            dut.io.instruction.func7.poke(0.U)
+            dut.io.instruction.reorder_pointer.poke(0.U)
+            dut.io.instruction.write_mode.poke(WriteMode.Register)
+            dut.io.instruction.instruction_pointer.poke(0.U)
+
+            dut.io.valid.poke(true.B)
+
+            dut.io.alu_ready.poke(true.B)
+
+            dut.io.broadcast_free_valid.poke(false.B)
+            dut.io.broadcast_free_register.poke(0.U)
+            dut.io.broadcast_free_value.poke(0.U)
+
+            dut.clock.step(1)
+
+            dut.io.valid.poke(false.B)
+
+            dut.io.broadcast_free_valid.poke(true.B)
+            dut.io.broadcast_free_register.poke(1.U)
+            dut.io.broadcast_free_value.poke(2.U)
+
+            dut.clock.step(1)
+
+            dut.io.valid.poke(true.B)
+            dut.io.instruction.rs1.poke(3.U)
+            dut.io.instruction.rs1_valid.poke(true.B)
+
+            dut.io.broadcast_free_valid.poke(false.B)
+
+            dut.io.alu_out_valid.expect(true.B)
+            dut.io.alu_out.opcode.expect("b0010011".U)
+
+            dut.io.broadcast_mark_valid.expect(true.B)
+            dut.io.broadcast_mark_register.expect(3.U)
+
+            dut.clock.step(1)
+
+            dut.io.valid.poke(false.B)
+
+            dut.io.instruction.rs1_valid.poke(true.B)
 
             dut.io.alu_out_valid.expect(false.B)
 
